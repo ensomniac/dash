23a58952fd2273ad2c0ee0d201b9b68186fd6c99
Ryan Martin <ryan@ensomniac.com>
2021-02-27 15:58:42 -0500

    Updated pdf tools

--- client/min/dash/dash.js
+++ client/min/dash/dash.js
@@ -2544,9 +2544,14 @@ function DashStyleView(){
 
 function DashPDFView(options){
     this.html = Dash.Gui.GetHTMLContext("", {"margin": d.Size.Padding});
+    console.log();
     options = options || {};
+    this.on_uploaded_callback = null;
     this.content_key = options["content_key"] || null;
     this.owner_email_list = options["owner_email_list"] || [];
+    if (options["binder"] && options["callback"]) {
+        this.on_uploaded_callback = options["callback"].bind(options["binder"]);
+    };
     this.upload_button = null;
     this.pages_area = $("<div></div>");
     this.setup_styles = function(){
@@ -2561,14 +2566,19 @@ function DashPDFView(options){
         this.params["key"] = this.content_key;
         this.params["token"] = d.Local.Get("token");
         this.upload_button.SetFileUploader(
-            "https://" + Dash.Context.domain + "/Artist",
+            "https://" + Dash.Context.domain + "/Api",
             this.params
         );
         this.html.append(this.upload_button.html);
         this.html.append(this.pages_area);
     };
-    this.upload_pdf = function(){
+    this.upload_pdf = function(response){
+        console.log(response);
         console.log("Upload pdf");
+        console.log(response);
+        if (this.on_uploaded_callback) {
+            this.on_uploaded_callback(response);
+        };
     };
     this.setup_styles();
 };
