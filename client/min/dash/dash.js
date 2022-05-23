75588c450eee999a8a8047f223f6c117e30bc27e
Andrew Stet <stetandrew@gmail.com>
2022-05-23 16:13:23 -0700

    revolving list update

--- client/min/dash/dash.js
+++ client/min/dash/dash.js
@@ -30655,6 +30655,66 @@ function DashLayoutListColumnConfig () {
             "css": css
         });
     };
+    this.AddCombo = function (label_text, combo_options, binder, callback, width=null, data_key="", can_edit=true, css=null, header_css=null) {
+        this.AddColumn(
+            label_text,
+            data_key,
+            can_edit,
+            width,
+            {
+                "type": "combo",
+                "options": {
+                    "label_text": label_text,
+                    "callback": callback,
+                    "binder": binder,
+                    "combo_options": combo_options
+                },
+                "css": css,
+                "header_css": header_css
+            }
+        );
+    };
+    this.AddIconButton = function (display_name, icon_name, binder, callback, size_mult=1.0, width=null, data_key="", can_edit=true, css=null, header_css=null) {
+        this.AddColumn(
+            display_name,
+            data_key,
+            can_edit,
+            width,
+            {
+                "type": "icon_button",
+                "options": {
+                    "icon_name": icon_name,
+                    "callback": callback,
+                    "binder": binder,
+                    "color": binder.color || Dash.Color.Light,
+                    "options": {
+                        "size_mult": size_mult
+                    }
+                },
+                "css": css,
+                "header_css": header_css
+            }
+        );
+    };
+    this.AddInput = function (label_text, binder, callback, width=null, data_key="", can_edit=true, css=null, header_css=null) {
+        this.AddColumn(
+            label_text,
+            data_key,
+            can_edit,
+            width,
+            {
+                "type": "input",
+                "options": {
+                    "placeholder_label": label_text,
+                    "callback" : callback,
+                    "binder": binder,
+                    "color": binder.color || Dash.Color.Light
+                },
+                "css": css,
+                "header_css": header_css
+            }
+        );
+    };
 }
 
 function DashLayoutListRowColumn (list_row, column_config_data, index, color=null) {
