d172b350a41286bcbd41b8e052f381885c47a363
Andrew Stet <stetandrew@gmail.com>
2022-10-24 20:41:04 -0700

    forgot index update

--- client/min/dash/dash.js
+++ client/min/dash/dash.js
@@ -33811,6 +33811,24 @@ function DashLayoutRevolvingListScrolling () {
     };
 }
 
+// This must be an abstraction to combine the two, since implementing the revolving list into the searchable list is not going to work favorably
+function DashLayoutSearchableRevolvingList (binder, column_config, color=null, include_header_row=false, row_options={}, get_data_for_key=null) {
+    this.list = new DashLayoutRevolvingList(binder, column_config, color, include_header_row, row_options, get_data_for_key);
+    this.input = new DashLayoutSearchableListInput(this.list, this.on_search.bind(this));
+    this.html = $("<div></div>");
+    this.setup_styles = function () {
+        this.list.html.css({
+            "position": ""
+        });
+        this.html.append(this.input.html);
+        this.html.append(this.list.html);
+    };
+    this.on_search = function (search_text) {
+        console.debug("TEST on search", search_text);  // TODO
+    };
+    this.setup_styles();
+}
+
 function DashLayoutSearchableList (binder, on_selection_callback, get_data_callback, on_row_draw_callback=null) {
     this.binder = binder;
     this.on_selection_callback = on_selection_callback.bind(this.binder);
