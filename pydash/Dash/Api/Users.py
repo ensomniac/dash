b7307362d5e9000671939843011e809170c65d94
Andrew Stet <stetandrew@gmail.com>
2022-12-22 16:05:44 -0800

    users updates

--- pydash/Dash/Api/Users.py
+++ pydash/Dash/Api/Users.py
@@ -17,80 +17,55 @@ class ApiUsers:
     DashContext: dict
     ParseParam: callable
     SetResponse: callable
+    ValidateParams: callable
 
     def __init__(self, execute_as_module, asset_path):
         self._execute_as_module = execute_as_module
         self._asset_path = asset_path
         self._on_init_callback = None
 
-        self.Add(self.r,               requires_authentication=False)
-        self.Add(self.reset,           requires_authentication=False)
-        self.Add(self.login,           requires_authentication=False)
-        self.Add(self.get_all,         requires_authentication=True)
-        self.Add(self.validate,        requires_authentication=False)
-        self.Add(self.upload_image,    requires_authentication=True)
-        self.Add(self.set_property,    requires_authentication=True)
-        self.Add(self.update_password, requires_authentication=True)
-
-    def reset(self):
-        from Dash.Users import Users as DashUsers
-
-        return self.SetResponse(DashUsers(self.Params, self.DashContext).Reset())
-
-    def r(self):
-        from Dash.Users import Users as DashUsers
-
-        return self.SetResponse(DashUsers(self.Params, self.DashContext).ResetResponse())
-
-    def login(self):
-        from Dash.Users import Users as DashUsers
-
-        response = self.SetResponse(DashUsers(self.Params, self.DashContext).Login())
-
-        return self.SetResponse(self.merge_addl_into_init(response))
+        self.Add(self.r,                    requires_authentication=False)
+        self.Add(self.reset,                requires_authentication=False)
+        self.Add(self.login,                requires_authentication=False)
+        self.Add(self.get_all,              requires_authentication=True)
+        self.Add(self.validate,             requires_authentication=False)
+        self.Add(self.update_pin,           requires_authentication=True)
+        self.Add(self.upload_image,         requires_authentication=True)
+        self.Add(self.set_property,         requires_authentication=True)
+        self.Add(self.update_password,      requires_authentication=True)
+        self.Add(self.validate_credentials, requires_authentication=True)
 
     def OnInit(self, callback):
         # When passed a callback, this function will be called whenever portal
         # init data is passed back, so that custom data can be included in the response.
         self._on_init_callback = callback
 
-    def validate(self):
-        from Dash.Users import Users as DashUsers
+    def r(self):
+        from Dash.Users import ResetResponse
 
-        response = DashUsers(self.Params, self.DashContext).Validate()
-
-        return self.SetResponse(self.merge_addl_into_init(response))
-
-    def merge_addl_into_init(self, response):
-
-        if "init" not in response:
-            return response
-
-        if not self._on_init_callback:
-            return response
-
-        additional = self._on_init_callback()
-
-        for key in additional:
-            response["init"][key] = additional[key]
-
-        return response
-
-    def update_password(self):
-        from Dash.Users import Users as DashUsers
-
-        return self.SetResponse(DashUsers(self.Params, self.DashContext).UpdatePassword())
-
-    def set_property(self):
-        from Dash.LocalStorage import SetProperty
-
-        return self.SetResponse(SetProperty(
-            dash_context=self.DashContext,
-            store_path="users",
-            obj_id=self.Params["obj_id"]
+        return self.SetResponse(ResetResponse(
+            request_params=self.Params,
+            dash_context=self.DashContext
         ))
 
-    # TODO - get rid of this code - it's been moved to Admin.py
+    def reset(self):
+        from Dash.Users import Reset
+
+        return self.SetResponse(Reset(
+            request_params=self.Params,
+            dash_context=self.DashContext
+        ))
+
+    def login(self):
+        from Dash.Users import Login
+
+        return self.SetResponse(self.merge_addl_into_init(Login(
+            use_pin=self.ParseParam("use_pin", bool, False),
+            request_params=self.Params,
+            dash_context=self.DashContext
+        )))
+
+    # TODO - get rid of this code - it's been moved to Admin.py (why is this not using Users.GetAll?)
     def get_all(self):
         from Dash.LocalStorage import Read
 
@@ -145,13 +120,26 @@ class ApiUsers:
 
         return self.SetResponse(response)
 
-    # TODO: Evaluate whether this is actually serving any purpose
-    def conform_user_data(self, user_data):
-        # Light wrapper to make sure certain things exist in returned user data
+    def validate(self):
+        from Dash.Users import Validate
 
-        user_data["conformed"] = True
+        return self.SetResponse(self.merge_addl_into_init(Validate(
+            request_params=self.Params,
+            dash_context=self.DashContext
+        )))
 
-        return user_data
+    def update_pin(self):
+        self.ValidateParams(["email", "pin"])
+
+        from Dash.Users import UpdatePIN
+
+        return self.SetResponse(UpdatePIN(
+            email=self.Params["email"],
+            pin=self.Params["pin"],
+            token_validation=self.ParseParam("token_validation", bool, True),
+            request_params=self.Params,
+            dash_context=self.DashContext
+        ))
 
     # TODO: Move this into the core Users.py module
     def upload_image(self):
@@ -177,3 +165,75 @@ class ApiUsers:
         Write(user_data_path, user_data)
 
         return self.SetResponse(user_data)
+
+    def set_property(self):
+        from Dash.LocalStorage import SetProperty
+
+        return self.SetResponse(SetProperty(
+            dash_context=self.DashContext,
+            store_path="users",
+            obj_id=self.Params["obj_id"]
+        ))
+
+    def update_password(self):
+        from Dash.Users import UpdatePassword
+
+        return self.SetResponse(UpdatePassword(
+            request_params=self.Params,
+            dash_context=self.DashContext
+        ))
+
+    def validate_credentials(self):
+        self.ValidateParams(["email", "password"])
+
+        response = self.get_credential_validation_data()
+
+        if not response.get("error"):
+            response["authenticated"] = True
+
+        return self.SetResponse(response)
+
+    # -------------------------------------------------------------------------------------
+
+    def merge_addl_into_init(self, response):
+        if "init" not in response:
+            return response
+
+        if not self._on_init_callback:
+            return response
+
+        additional = self._on_init_callback()
+
+        for key in additional:
+            response["init"][key] = additional[key]
+
+        return response
+
+    # TODO: Evaluate whether this is actually serving any purpose
+    def conform_user_data(self, user_data):
+        # Light wrapper to make sure certain things exist in returned user data
+
+        user_data["conformed"] = True
+
+        return user_data
+
+    def get_credential_validation_data(self):
+        from Dash.Users import ValidateCredentials
+
+        response = {
+            "email": self.Params["email"],
+            "authenticated": False
+        }
+
+        validation = ValidateCredentials(
+            email=self.Params["email"],
+            password=self.Params["password"],
+            return_login_dict=True,  # Just to get the error messages
+            request_params=self.Params,
+            dash_context=self.DashContext
+        )
+
+        if validation.get("error"):
+            response["error"] = validation["error"]
+
+        return response
