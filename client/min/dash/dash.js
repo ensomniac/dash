04efef1809937b72d29b0402accc8a5ac2c266a7
Andrew Stet <stetandrew@gmail.com>
2023-02-19 14:01:20 -0800

    prop box and user view updates

--- client/min/dash/dash.js
+++ client/min/dash/dash.js
@@ -33625,7 +33625,7 @@ function DashGuiPropertyBoxInterface () {
         return row;
     };
     this.AddLabel = function (text, color=null) {
-        var header = new Dash.Gui.Header(text, color);
+        var header = new Dash.Gui.Header(text, color || this.color);
         header.html.css({
             "margin-left": Dash.Size.Padding * 2
         });
@@ -33633,7 +33633,7 @@ function DashGuiPropertyBoxInterface () {
         return header;
     };
     this.AddText = function (text, color=null) {
-        var label = this.AddLabel(text, false, color);
+        var label = this.AddLabel(text, false, color || this.color);
         label.border.remove();
         label.label.css({
             "font-family": "sans_serif_normal",
@@ -33645,6 +33645,7 @@ function DashGuiPropertyBoxInterface () {
         this.html.append(label.html);
         return label;
     };
+    // TODO: this should've originally been setup to be directly connected to this property box's set_data function
     this.AddCheckbox = function (
         local_storage_key="", default_state=true, color=null, hover_hint="Toggle", binder=null, callback=null,
         label_text="", label_first=true, include_border=false, read_only=false, icon_redraw_styling=null, highlight_row=true
@@ -33656,7 +33657,7 @@ function DashGuiPropertyBoxInterface () {
         var checkbox = new Dash.Gui.Checkbox(
             local_storage_key,
             default_state,
-            color,
+            color || this.color,
             hover_hint,
             binder,
             callback && highlight_row ? (function (self) {
@@ -41638,6 +41639,7 @@ function DashPDFView (options) {
 function DashUserView (user_data=null, options={}, view_mode="settings") {
     this.html = Dash.Gui.GetHTMLContext("", {"margin": Dash.Size.Padding});
     this.user_profile = new Dash.Layout.UserProfile(user_data, options, view_mode);
+    this.property_box = this.user_profile.property_box;
     this.html.append(this.user_profile.html);
 }
 
