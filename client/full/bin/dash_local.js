913b6a4ef5d73d03cef77c43826573e01cfde4be
Andrew Stet <stetandrew@gmail.com>
2023-05-22 20:35:15 -0700

    local update

--- client/full/bin/dash_local.js
+++ client/full/bin/dash_local.js
@@ -1,22 +1,22 @@
 function DashLocal (context) {
     this.context = context;
 
-    this.Set = function (key, value) {
+    this.Set = function (key, value, session=false) {
         if (key.indexOf(this.context["asset_path"] + "_") !== 0) {
             key = this.context["asset_path"] + "_" + key;
         }
 
-        return localStorage.setItem(key, value);
+        return session ? sessionStorage.setItem(key, value) : localStorage.setItem(key, value);
     };
 
     // If bool_default is provided, the value will be
     // parsed as a bool and default to the value provided
-    this.Get = function (key, bool_default=null) {
+    this.Get = function (key, bool_default=null, session=false) {
         if (key.indexOf(this.context["asset_path"] + "_") !== 0) {
             key = this.context["asset_path"] + "_" + key;
         }
 
-        var value = localStorage.getItem(key);
+        var value = session ? sessionStorage.getItem(key) : localStorage.getItem(key);
 
         if (bool_default === true) {
             return ["true", true, null, ""].includes(value);
