86d38f8299ac45c6dfdcc0f61c9eaa2964f0d37e
Andrew Stet <stetandrew@gmail.com>
2025-02-13 16:09:33 -0800

    DashMobileSearchableCombo class conv and cleanup

--- client/full/bin/mobile/dash_mobile_searchable_combo.js
+++ client/full/bin/mobile/dash_mobile_searchable_combo.js
@@ -1,38 +1,42 @@
-function DashMobileSearchableCombo (
-    color=null, options={}, placeholder_text="", binder=null, on_submit_cb=null, on_change_cb=null
-) {
-    this.color = color || (binder && binder.color ? binder.color : Dash.Color.Light);
-    this.options = options;  // Format: {id: label}
-    this.placeholder_text = placeholder_text;
-    this.binder = binder;
-    this.on_submit_cb = binder && on_submit_cb ? on_submit_cb.bind(binder) : on_submit_cb;
-    this.on_change_cb = binder && on_change_cb ? on_change_cb.bind(binder) : on_change_cb;
+class DashMobileSearchableCombo {
+    constructor(
+        color=null, options={}, placeholder_text="", binder=null, on_submit_cb=null, on_change_cb=null
+    ) {
+        this.color = color || (binder && binder.color ? binder.color : Dash.Color.Light);
+        this.options = options;  // Format: {id: label}
+        this.placeholder_text = placeholder_text;
+        this.binder = binder;
+        this.on_submit_cb = binder && on_submit_cb ? on_submit_cb.bind(binder) : on_submit_cb;
+        this.on_change_cb = binder && on_change_cb ? on_change_cb.bind(binder) : on_change_cb;
 
-    this.label = null;
-    this.disabled = false;
-    // this.option_rows = [];
-    this.clear_button = null;
-    // this.on_change_delay_ms = 0;
-    this.html = $("<div></div>");
-    // this.on_change_timeout = null;
-    this.id = "DashMobileSearchableCombo_" + Dash.Math.RandomID();
-    this.datalist = $("<datalist></datalist", {"id": this.id});
+        this.label = null;
+        this.disabled = false;
+        // this.option_rows = [];
+        this.html = $("<div>");
+        this.clear_button = null;
+        // this.on_change_delay_ms = 0;
+        // this.on_change_timeout = null;
+        this.id = "DashMobileSearchableCombo_" + Dash.Math.RandomID();
+        this.datalist = $("<datalist>", {"id": this.id});
 
-    this.input = $(
-        "<input/>",
-        {
-            "list": this.id,
-            "class": this.color.PlaceholderClass,
-            "placeholder": this.placeholder_text,
-            "inputmode": "search",
-            "autocomplete": "off",
-            "autocorrect": "off",
-            "autocapitalize": "off",
-            "spellcheck": false
-        }
-    );
+        this.input = $(
+            "<input>",
+            {
+                "list": this.id,
+                "class": this.color.PlaceholderClass,
+                "placeholder": this.placeholder_text,
+                "inputmode": "search",
+                "autocomplete": "off",
+                "autocorrect": "off",
+                "autocapitalize": "off",
+                "spellcheck": false
+            }
+        );
 
-    this.setup_styles = function () {
+        this.setup_styles();
+    }
+
+    setup_styles () {
         var shared_css = {
             "box-sizing": "border-box",
             "color": this.color.Text,
@@ -59,32 +63,92 @@ function DashMobileSearchableCombo (
         this.html.append(this.input);
 
         this.setup_connections();
-    };
+    }
 
-    this.GetID = function (allow_none=true) {
-        var label = this.GetLabel();
+    SetOnChangeDelayMs(ms) {
+        this.on_change_delay_ms = parseInt(ms);
+    }
 
-        for (var id in this.options) {
-            if (this.options[id].toString().toLowerCase() === label.toString().toLowerCase()) {
-                if (id === "none" && !allow_none) {
-                    return null;
-                }
-
-                return id;
+    AddOption(id, label, _check=true, _from_filter=false) {
+        if (!_from_filter) {
+            if (_check && this.options[id]) {
+                return;
             }
+
+            this.options[id] = label;
         }
 
-        return null;
-    };
+        // Unlike the select element, the datalist does not allow option elements
+        // to contain both a value and a label, so for us to get the ID after a
+        // selection is made, we loop through the options and match the current value (label)
+        var row = $("<option>",{"value": label});
 
-    this.GetLabel = function () {
-        return this.input.val();
-    };
+        row.css({
+            "height": Dash.Size.RowHeight
+        });
+
+        // if (!_from_filter) {
+        //     this.option_rows.push(row);
+        // }
+
+        this.datalist.append(row);
+    }
+
+    SetOptions(options={}) {
+        this.datalist.empty();
+
+        this.options = options;
+
+        this.add_options();
+    }
+
+    GetOptions () {
+        return this.options;
+    }
+
+    SetLabelByID(id) {
+        if (!(id in this.options)) {
+            Dash.Log.Warn("ID not in options:", id);
+
+            return;
+        }
+
+        this.SetLabel(this.options[id]);
+    }
+
+    AddLabel(text) {
+        if (this.label) {
+            return this.label;
+        }
+
+        this.label = $("<div>", {"text": text});
+
+        this.label.css({
+            "position": "absolute",
+            "font-family": "sans_serif_bold",
+            "font-size": "80%",
+            "color": this.color.StrokeLight,
+            "top": -Dash.Size.Padding * 0.8,
+            "left": Dash.Size.Padding * 0.1
+        });
+
+        this.html.append(this.label);
+
+        return this.label;
+    }
+
+    EnableResetInvalidOnBlur () {
+        this.input.on("blur", () => {
+            if (!this.GetID()) {
+                this.input.val("");
+            }
+        });
+    }
 
     // Sometimes, setting the value like this causes the dropdown icon to disappear,
     // so attempting a delayed value set, then a delayed focus and delayed blur
     // to attempt to refresh the state of the input after value is set
-    this.SetLabel = function (text) {
+    SetLabel(text) {
         setTimeout(
             () => {
                 this.input.val(text);
@@ -105,114 +169,74 @@ function DashMobileSearchableCombo (
             },
             100
         );
-    };
+    }
 
-    this.SetLabelByID = function (id) {
-        if (!(id in this.options)) {
-            Dash.Log.Warn("ID not in options:", id);
-
+    Enable () {
+        if (!this.disabled) {
             return;
         }
 
-        this.SetLabel(this.options[id]);
-    };
+        this.disabled = false;
 
-    this.SetOnChangeDelayMs = function (ms) {
-        this.on_change_delay_ms = parseInt(ms);
-    };
+        this.html.css({
+            "opacity": 1,
+            "pointer-events": "auto",
+            "user-select": "auto"
+        });
+    }
 
-    this.GetOptions = function () {
-        return this.options;
-    };
-
-    this.SetOptions = function (options={}) {
-        this.datalist.empty();
-
-        this.options = options;
-
-        this.add_options();
-    };
-
-    this.AddOption = function (id, label, _check=true, _from_filter=false) {
-        if (!_from_filter) {
-            if (_check && this.options[id]) {
-                return;
-            }
-
-            this.options[id] = label;
+    // There might be a better way to do this for a datalist element, but
+    // this is a quick thing for now since I have limited time
+    // - maybe update later, maybe not a big deal
+    Disable(opacity=0.5) {
+        if (this.disabled) {
+            return;
         }
 
-        // Unlike the select element, the datalist does not allow option elements
-        // to contain both a value and a label, so for us to get the ID after a
-        // selection is made, we loop through the options and match the current value (label)
-        var row = $("<option></option>",{"value": label});
+        this.disabled = true;
 
-        row.css({
-            "height": Dash.Size.RowHeight
+        this.html.css({
+            "opacity": opacity,
+            "pointer-events": "none",
+            "user-select": "none"
         });
+    }
 
-        // if (!_from_filter) {
-        //     this.option_rows.push(row);
-        // }
+    GetID(allow_none=true) {
+        var label = this.GetLabel();
 
-        this.datalist.append(row);
-    };
-
-    this.EnableResetInvalidOnBlur = function () {
-        (function (self) {
-            self.input.on("blur", function () {
-                if (!self.GetID()) {
-                    self.input.val("");
+        for (var id in this.options) {
+            if (this.options[id].toString().toLowerCase() === label.toString().toLowerCase()) {
+                if (id === "none" && !allow_none) {
+                    return null;
                 }
-            });
-        })(this);
-    };
 
-    this.AddLabel = function (text) {
-        if (this.label) {
-            return this.label;
+                return id;
+            }
         }
 
-        this.label = $("<div>" + text + "</div>");
+        return null;
+    }
 
-        this.label.css({
-            "position": "absolute",
-            "font-family": "sans_serif_bold",
-            "font-size": "80%",
-            "color": this.color.StrokeLight,
-            "top": -Dash.Size.Padding * 0.8,
-            "left": Dash.Size.Padding * 0.1
-        });
+    GetLabel () {
+        return this.input.val();
+    }
 
-        this.html.append(this.label);
-
-        return this.label;
-    };
-
-    this.AddClearButton = function () {
+    AddClearButton () {
         if (this.clear_button) {
             return this.clear_button;
         }
 
-        (function (self) {
-            self.clear_button = new Dash.Gui.IconButton(
-                "close_circle",
-                function () {
-                    self.SetLabel("");
-
-                    requestAnimationFrame(function () {
-                        self.input.trigger("focus");
-                        self.input.trigger("click");
-                    });
-                },
-                self,
-                self.color,
-                {
-                    "container_size": Dash.Size.RowHeight,
-                    "size_mult": 0.6
-                }
-            );
-        })(this);
+        this.clear_button = new Dash.Gui.IconButton(
+            "close_circle",
+            this.clear,
+            this,
+            this.color,
+            {
+                "container_size": Dash.Size.RowHeight,
+                "size_mult": 0.6
+            }
+        );
 
         this.clear_button.SetIconColor(Dash.Color.Mobile.AccentPrimary);
 
@@ -227,65 +251,14 @@ function DashMobileSearchableCombo (
         this.html.append(this.clear_button.html);
 
         return this.clear_button;
-    };
-
-    // There might be a better way to do this for a datalist element, but
-    // this is a quick thing for now since I have limited time
-    // - maybe update later, maybe not a big deal
-    this.Disable = function (opacity=0.5) {
-        if (this.disabled) {
-            return;
-        }
-
-        this.disabled = true;
-
-        this.html.css({
-            "opacity": opacity,
-            "pointer-events": "none",
-            "user-select": "none"
-        });
-    };
-
-    this.Enable = function () {
-        if (!this.disabled) {
-            return;
-        }
-
-        this.disabled = false;
-
-        this.html.css({
-            "opacity": 1,
-            "pointer-events": "auto",
-            "user-select": "auto"
-        });
-    };
-
-    this.set_width = function (width, set_input=false, min_width=null, max_width=null) {
-        var css = {
-            "width": width,
-            "min-width": min_width || width,
-            "max-width": max_width || width
-        };
-
-        this.html.css(css);
-
-        if (set_input) {
-            this.input.css(css);
-        }
-    };
-
-    this.add_options = function (_from_filter=false) {
-        for (var id in this.options) {
-            this.AddOption(id, this.options[id], false, _from_filter);
-        }
-    };
+    }
 
     // Datalists have their own built-in native filtering, but it returns matches
     // for anything that includes the characters typed in the input, so this
     // overwrites it. For example, typing the letter "m" should (typically) return
     // results that start with "m", but datalists return any result that includes
     // the letter "m", which doesn't feel logical to the user, in most cases.
-    this.filter_datalist = function () {
+    filter_datalist () {
         var id;
         var label;
         var added_ids = [];
@@ -360,9 +333,95 @@ function DashMobileSearchableCombo (
                 return;
             }
         }
-    };
+    }
 
-    // this.on_change = function () {
+    set_width(width, set_input=false, min_width=null, max_width=null) {
+        var css = {
+            "width": width,
+            "min-width": min_width || width,
+            "max-width": max_width || width
+        };
+
+        this.html.css(css);
+
+        if (set_input) {
+            this.input.css(css);
+        }
+    }
+
+    clear () {
+        this.SetLabel("");
+
+        requestAnimationFrame(() => {
+            this.input.trigger("focus");
+            this.input.trigger("click");
+        });
+    }
+
+    add_options(_from_filter=false) {
+        for (var id in this.options) {
+            this.AddOption(id, this.options[id], false, _from_filter);
+        }
+    }
+
+    setup_connections () {
+        this.input.on("change", () => {
+            // Since this is linked to the datalist, the change event only triggers
+            // when a selection is made, whether that's by clicking an option or
+            // typing an option and selecting it using the arrow keys and enter key
+            if (this.on_submit_cb) {
+                var id = this.GetID();
+
+                if (id) {
+                    this.on_submit_cb(id);
+                }
+            }
+        });
+
+        this.input.on("input", () => {
+            // Since we're overriding the datalist's default filtering and
+            // that default filtering doesn't get delayed, delaying the
+            // custom filtering causes both default and custom filtering to
+            // occur, which is visibly noticeable. This doesn't appear to
+            // be necessary anyway for performance, so not a big deal.
+            // this.on_change();
+
+            this.filter_datalist();
+
+            if (this.on_change_cb) {
+                this.on_change_cb();
+            }
+        });
+
+        this.input.on("click", (event, reclick=false) => {
+            if (reclick) {
+                // Force redraw of datalist
+                // this.option_rows.Last().detach();
+                //
+                // requestAnimationFrame(function () {
+                //     this.datalist.append(this.option_rows.Last());
+                // });
+
+                return;
+            }
+
+            this.trigger_reclick();
+        });
+    }
+
+    trigger_reclick () {
+        setTimeout(
+            () => {
+                // If the list is long, the list will cover the virtual
+                // keyboard unless re-clicked after initial draw
+                this.input.trigger("focus");
+                this.input.trigger("click", [true]);
+            },
+            300
+        );
+    }
+
+    // on_change () {
     //     if (!this.on_change_delay_ms) {
     //         this.filter_datalist();
     //
@@ -379,79 +438,15 @@ function DashMobileSearchableCombo (
     //         this.on_change_timeout = null;
     //     }
     //
-    //     (function (self) {
-    //         self.on_change_timeout = setTimeout(
-    //             function () {
-    //                 self.filter_datalist();
+    //     this.on_change_timeout = setTimeout(
+    //         () => {
+    //             this.filter_datalist();
     //
-    //                 if (self.on_change_cb) {
-    //                     self.on_change_cb();
-    //                 }
-    //             },
-    //             self.on_change_delay_ms
-    //         );
-    //     })(this);
-    // };
-
-    this.setup_connections = function () {
-        (function (self) {
-            self.input.on("change", function () {
-                // Since this is linked to the datalist, the change event only triggers
-                // when a selection is made, whether that's by clicking an option or
-                // typing an option and selecting it using the arrow keys and enter key
-                if (self.on_submit_cb) {
-                    var id = self.GetID();
-
-                    if (id) {
-                        self.on_submit_cb(id);
-                    }
-                }
-            });
-
-            self.input.on("input", function () {
-                // Since we're overriding the datalist's default filtering and
-                // that default filtering doesn't get delayed, delaying the
-                // custom filtering causes both default and custom filtering to
-                // occur, which is visibly noticeable. This doesn't appear to
-                // be necessary anyway for performance, so not a big deal.
-                // self.on_change();
-
-                self.filter_datalist();
-
-                if (self.on_change_cb) {
-                    self.on_change_cb();
-                }
-            });
-
-            self.input.on("click", function (event, reclick=false) {
-                if (reclick) {
-                    // Force redraw of datalist
-                    // self.option_rows.Last().detach();
-                    //
-                    // requestAnimationFrame(function () {
-                    //     self.datalist.append(self.option_rows.Last());
-                    // });
-
-                    return;
-                }
-
-                self.trigger_reclick();
-            });
-        })(this);
-    };
-
-    this.trigger_reclick = function () {
-        (function (self) {
-            setTimeout(
-                function () {
-                    // If the list is long, the list will cover the virtual keyboard unless re-clicked after initial draw
-                    self.input.trigger("focus");
-                    self.input.trigger("click", [true]);
-                },
-                300
-            );
-        })(this);
-    };
-
-    this.setup_styles();
+    //             if (this.on_change_cb) {
+    //                 this.on_change_cb();
+    //             }
+    //         },
+    //         this.on_change_delay_ms
+    //     );
+    // }
 }
